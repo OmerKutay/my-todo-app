f90d42a2fd69c865e04270665ec4ad39f72b61d7
admin <admin@PAINKILLER06>
2023-10-08 19:26:02 +0300

    Initial Commit

--- web.py
+++ web.py
@@ -10,7 +10,7 @@ def add_todo():
     functions.write_todos(todos)
 
 
-st.title("My Todo App")
+st.title("ÖYKÜ ÖMER WEB SAYFASI")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity")
 
